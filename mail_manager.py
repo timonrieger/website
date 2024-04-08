1b94fb20945d372dbb69b662ee1e1145b2e9fb78
Timon Rieger <timon@riegerx.de>
2024-04-08 16:33:30 -0700

    improve confirmation email

--- mail_manager.py
+++ mail_manager.py
@@ -12,7 +12,12 @@ class MailManagaer():
         characters = string.ascii_letters + string.digits
         self.confirmation_token = ''.join(secrets.choice(characters) for i in range(20))
         confirmation_link = f"http://127.0.0.1:5000/confirm/{to_email}?token={self.confirmation_token}&form={form}"
-        message = f"Subject: Confirmation Link\n\nClick this link to verify: {confirmation_link}"
+        message = (f"Subject: Account Confirmation Link\n\n"
+                   f"Hello,\n\n"
+                   f"Thank you for signing up! To complete your registration, please click the link below:\n\n{confirmation_link}\n\n"
+                   f"If you did not request this registration or have any questions, please ignore this message.\n\n"
+                   f"Best regards,\n\nTimon Rieger")
+
         # Send the email
         with smtplib.SMTP("smtp.gmail.com", port=587) as server:
                 server.starttls()
