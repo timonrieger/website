6271022aec2f3fc4ef28995ecaf81b7c9f063657
Timon Rieger <timon@riegerx.de>
2024-04-29 15:48:58 +0200

    intial commit

---
+++ secret_keys.py
@@ -0,0 +1,15 @@
+### Air Nomad Society ###
+TEQUILA_API_KEY = "SoUbJl908JzpD4Y4wOPhJrpgV5FhY63k"
+SHEETY_BEARER = "djgnglmafwm"
+ANS_EMAIL = "airnomadsociety@gmail.com"
+ANS_MAIL_PASSWORD = "giqm bkqh hfde kaew"
+TRAVEL_DATA = "https://api.npoint.io/9e625c836edf8e4047a8"
+### Flashback Playlists ###
+SPOTIFY_CLIENT_ID = "a49ca241980f4d2680217f6a8a6e79aa"
+SPOTIFY_SECRET = "7fed768081894c3a80ff910f837d0de7"
+### Flask Server ###
+FLASK_SECRET_KEY = "tfwbwU#2005_flask"
+GMAIL_EMAIL = "timonriegerx@gmail.com"
+GMAIL_PASSWORD = "grba gxjz zxpj zgjq"
+RECAPTCHA_PUBLIC_KEY = "6LfWF8cpAAAAAJSd49GC6L1ZJiwgoYVHmfEd8HGz"
+RECAPTCHA_PRIVATE_KEY = "6LfWF8cpAAAAAGLRvpo4p-qkG0ZZ1AwhV_0iAduX"
