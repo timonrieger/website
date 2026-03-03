8752873c529982599240109a92e49b651be201b7
timonrieger <mail@timonrieger.de>
2026-03-03 13:21:15 +0100

    fix

--- scripts/install-go.sh
+++ scripts/install-go.sh
@@ -13,10 +13,12 @@ main() {
 
   # Install Go
   echo "Installing Go ${GO_VERSION}..."
+  mkdir -p "${HOME}/.local"
   curl -sLJO "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz"
   tar -C "${HOME}/.local" -xf "go${GO_VERSION}.linux-amd64.tar.gz"
   rm "go${GO_VERSION}.linux-amd64.tar.gz"
   export PATH="${HOME}/.local/go/bin:${PATH}"
+  go version
 }
 
 set -euo pipefail
