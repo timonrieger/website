0bcec7fbcd32393fc749de8a33e2dac0a15495c5
timonrieger <mail@timonrieger.de>
2026-03-03 13:17:15 +0100

    install go

---
+++ scripts/install-go.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#------------------------------------------------------------------------------
+# @file
+# This file installs go, so that go modules can be used.
+#------------------------------------------------------------------------------
+
+main() {
+
+  GO_VERSION=1.25.4
+
+  export TZ=Europe/Zurich
+
+  # Install Go
+  echo "Installing Go ${GO_VERSION}..."
+  curl -sLJO "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz"
+  tar -C "${HOME}/.local" -xf "go${GO_VERSION}.linux-amd64.tar.gz"
+  rm "go${GO_VERSION}.linux-amd64.tar.gz"
+  export PATH="${HOME}/.local/go/bin:${PATH}"
+}
+
+set -euo pipefail
+main "$@"
