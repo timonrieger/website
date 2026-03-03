8049c9b5beeae4177528d76af6849817aadbf78d
Timon <mail@timonrieger.de>
2026-03-03 13:47:17 +0100

    Add backlinks plugin (#4)
    * Add Go module support and integrate backlinks feature
    
    * install go
    
    * fix
    
    * align go version
    
    * this
    
    * add go ignore

---
+++ scripts/install-go.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#------------------------------------------------------------------------------
+# @file
+# This file installs go, so that go modules can be used.
+#------------------------------------------------------------------------------
+
+main() {
+  export TZ=Europe/Zurich
+
+  # Install Go
+  echo "Installing Go ${GO_VERSION}..."
+  curl -sLJO "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz"
+  tar -C "${HOME}/.local" -xf "go${GO_VERSION}.linux-amd64.tar.gz"
+  rm "go${GO_VERSION}.linux-amd64.tar.gz"
+  export PATH="${HOME}/.local/go/bin:${PATH}"
+  go version
+}
+
+set -euo pipefail
+main "$@"
